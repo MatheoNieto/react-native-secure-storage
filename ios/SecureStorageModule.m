967021aff0b850c4e1b91808df073776b330df4c
Mateo Nieto <matheo.developer@gmail.com>
2025-08-31 00:08:13 -0500

    error importing the module ios

--- ios/SecureStorageModule.m
+++ ios/SecureStorageModule.m
@@ -1,6 +1,6 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(StorageModule, NSObject)
+@interface RCT_EXTERN_MODULE(SecureStorageModule, NSObject)
 
 RCT_EXTERN_METHOD(saveString:(NSString *)key
                   value:(NSString *)value
@@ -11,24 +11,6 @@ RCT_EXTERN_METHOD(loadString:(NSString *)key
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(saveJSON:(NSString *)key
-                  jsonString:(NSString *)jsonString
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(loadJSON:(NSString *)key
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(setItem:(NSString *)key
-                  value:(NSString *)value
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(getItem:(NSString *)key
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
 RCT_EXTERN_METHOD(removeItem:(NSString *)key
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
@@ -36,11 +18,5 @@ RCT_EXTERN_METHOD(removeItem:(NSString *)key
 RCT_EXTERN_METHOD(removeAll:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(hasItem:(NSString *)key
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-
-RCT_EXTERN_METHOD(getAllKeys:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
 
 @end
