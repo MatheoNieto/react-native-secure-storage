9917b13eea1ab1d640765f21d5d6c9a7dbf083a9
Mateo Nieto <matheo.developer@gmail.com>
2025-09-01 15:13:01 -0500

    error importing in android secure storare

--- android/src/main/java/com/rnstorage/SecureStorageModule.kt
+++ android/src/main/java/com/rnstorage/SecureStorageModule.kt
@@ -1,12 +1,9 @@
 package com.rnstorage
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.WritableNativeArray
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
+import com.facebook.react.bridge.*
 
 class SecureStorageModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     private val secureStorage = SecureStorageManager(reactContext)
